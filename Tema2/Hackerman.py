63e04e978b0d85137f2c238cbad800f5b31beea4
Sergio <sermmoba8@gmail.com>
2024-10-15 11:41:23 +0200

    Hackerman

--- Tema2/Hackerman.py
+++ Tema2/Hackerman.py
@@ -1,30 +1,46 @@
+import copy
 from collections import deque
 
 
 #BFS algorithm definition
 
-def bfs_aux(g,v,visited):
+
+def eliminar_nodo(g,v):
+    grafo_copia = copy.deepcopy(g)
+    grafo_copia[v]= []
+    n= len(g)
+    for j in range(n):
+        if v in grafo_copia[j]:
+            grafo_copia[j].remove(v)
+    return grafo_copia
+
+def bfs_aux(g,start,visited):
     q= deque()
-    visited[v]= True
-    q.append(v)
+    visited[start]= True
+    q.append(start)
     while q:
-        aux = q.popleft()
-        for adj in g[aux]:
+        auxN = q.popleft()
+        for adj in g[auxN]:
             if not visited[adj]:
                 q.append(adj)
                 visited[adj] = True
 
 
 
-def bfs(g):
+def bfs(g,costeReparacion):
     n=len(g)
-    visited=[False]*(n)
+    costeTotal =0
     for v in range(n):
-        if not visited[v]:
-            bfs_aux(g,v,visited)
-
-
-
+        grafoCopia= eliminar_nodo(g,v)
+        if v!= 0:
+            start = 0
+        else:
+            start = 1
+        visited = [False] * n
+        bfs_aux(grafoCopia,start,visited)
+        if not all(visited[i] or i==v for i in range(n)): # Si la red esta desconectada, sumamos el precio de reparacion
+            costeTotal += costeReparacion[v]
+    return costeTotal
 
 
 
@@ -33,10 +49,13 @@ n,m = map(int,input().strip().split())
 g=[]
 costeReparacion=[]
 for i in range(n):
-    coste=int(input("Introduce el coste de reparar el nodo {i}:"))
+    coste=int(input(f"Introduce el coste de reparar el nodo {i}:"))
     costeReparacion.append(coste)
     g.append([])
 for j in range(m):
     a,b = map(int,input().strip().split())
     g[a].append(b)
     g[b].append(a)
+
+costeSistema= bfs(g,costeReparacion)
+print(costeSistema)
