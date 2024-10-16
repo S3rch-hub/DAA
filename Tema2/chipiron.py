21b0caff4bb4ab43bd537cd4a8fbfee1ddd400fb
smoralesssss <sermoba8@gmail.com>
2024-10-16 12:37:44 +0200

    Hackerman with 2 funtions in order to copy graph

--- Tema2/chipiron.py
+++ Tema2/chipiron.py
@@ -1,10 +1,11 @@
 from collections import deque
 
 #BFS algorithimic (Busqueda en anchura)
-def bfsAux(g, visited, v):
+def bfsAux(g, visited, v,distancia,turno):
     q = deque()
     visited[v] = True
     q.append(v)
+    distancia += 1
     while q:
         aux = q.popleft()
         for adj in g[aux]:
@@ -16,17 +17,19 @@ def bfsAux(g, visited, v):
 def bfs(g):
     n = len(g)
     visited = [False] * n
-    ncc= 0
-    for v in range(0, n):
+    turno = 1
+    distancia_min = 0
+    for v in range(n):
         if not visited[v]:
-            bfsAux(g, visited, v)
-            ncc += 1
-    return ncc
+            bfsAux(g, visited, v,distancia_min,turno)
+    return distancia_min
 
 #Data definition
 
 n,m = map(int,input().strip().split())
 g=[]
-for i in range (n):
-    g.append([])
+for i in range(n):
+    a = list(map(int,input().strip().split()))
+    g.append(a)
+print(g)
 
