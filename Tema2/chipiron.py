a1a1d895d001478fdc44aa575639d0feed93f9e2
Sergio <sermmoba8@gmail.com>
2024-10-17 10:54:53 +0200

    juego del chipiron

--- Tema2/chipiron.py
+++ Tema2/chipiron.py
@@ -2,23 +2,34 @@ from collections import deque
 
 #BFS algorithimic (Busqueda en anchura)
 
-def bfs(g,x,y,turno):
-    n = len(g)
-    distancia_min = 0
+def bfs(g,x,y,turno,distancia_min):
     if g[x][y]== 2:
         return distancia_min
-    encontrado=0
     q =deque()
-    while not encontrado:
-        if turno %2 ==1:
+    q.append((x,y,turno,distancia_min))
+    visited = set()
+    visited.add((x,y))
+    while q:
+        x,y,turno,distancia_min = q.popleft()
+        if g[x][y] == 2:
+            return distancia_min
+        direcciones= [(1,0),(0,1),(-1,0),(0,-1)]
 
-            q.append((x+1,y))
-            q.append((x,y+1))
-            while q:
-                x,y =q.popleft()
-                distancia_min += 1
-                if g[x][y] == 2:
-                    encontrado =1
+        if turno %2 == 1: # En los turnos impares
+            for auxX,auxY in direcciones:
+                nx,ny= x+auxX,y+auxY
+                if 0<=nx<n and 0<=ny<m and (nx,ny) not in visited:
+                    visited.add((nx,ny))
+                    q.append((nx,ny,turno + 1,distancia_min + 1))
+        else: # En los turnos pares
+            for auxX,auxY in direcciones:
+                nx,ny= x + auxX,y + auxY
+                if 0<=nx<n and 0<=ny<m and (nx,ny) not in visited:
+                    if g[nx][ny] ==0 or g[nx][ny] == 2:
+                        visited.add((nx,ny))
+                        q.append((nx,ny,turno +1, distancia_min + 1))
+
+
 
 
 
@@ -34,9 +45,10 @@ g=[]
 for i in range(n):
     a = list(map(int,input().strip().split()))
     g.append(a)
-x= 0
-y = 0
+distancia_min = 0
+origenX = 0
+origenY = 0
 turno =1
-bfs(g,x,y,turno)
-print(g)
+sol = bfs(g,origenX,origenY,turno,distancia_min)
+print(sol)
 
