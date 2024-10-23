cd279975c8381f56db3615b9f896be40f6f5bf43
Sergio <sermmoba8@gmail.com>
2024-10-23 10:56:16 +0200

    leyendo comics done

--- Tema2/LeyendoComics.py
+++ Tema2/LeyendoComics.py
@@ -1,38 +1,23 @@
-
+import heapq
 from collections import deque
 
 
-def topSortVisited(data,k):
-    data["estado"][k] = "VISITADO"
-    data["tiempo"] += 1
-    data["d"][k] = data["tiempo"]
-    for adj in data["grafo"][k]: # Para cada adyacente, vemos si esta visitado
-        if data["estado"][adj] == "NO VISITADO":
-            topSortVisited(data,adj)
-    data["estado"][k]= "TERMINADO" # Cuando nos hayamos recorrido la lista de adyacencia del nodo k, este ya lo podemos dar por terminado y lo metemos en la sol
-    data["tiempo"] += 1
-    data["f"][k] = data["tiempo"]
-    data["sol"].appendleft(k)
+def topSort(g,grados):
+    sol =[]
+    heap=[]
 
-def topSort(g):
-    data = {
-        "grafo" : g,
-        "estado" : dict(),
-        "d" : dict(), # Tiempo en el que se descubre por primera vez el nodo
-        "f" : dict(), # Tiempo en el que se terminan de explorar todas las dependencias de un nodo
-        "tiempo" : 0, # Contandor que se incrementa cada vez que se descubre o finaliza un nodo
-        "sol" : deque()
+    for k in range(len(grados)):
+        if grados[k] ==0:
+            heapq.heappush(heap,k)
 
-    }
-
-    for k in g.keys(): # Inicializamos los estados a NO VISITADO
-        data["estado"][k] = "NO VISITADO"
-        data["d"][k] = 0
-        data["f"][k] = 0
-    for k in g.keys(): # Para cada nodo, verificamos si no lo hemos visitado
-        if data["estado"][k] == "NO VISITADO":
-            topSortVisited(data,k)
-    return sorted(list(data["sol"]))
+    while heap:
+        aux = heapq.heappop(heap)
+        sol.append(aux)
+        for adj in g[aux]:
+            grados[adj] -=1
+            if grados[adj] == 0:
+                heapq.heappush(heap,adj)
+    return sol
 
 
 
@@ -40,12 +25,14 @@ def topSort(g):
 
 n,m=map(int,input().strip().split())
 g=dict()
+grados=[0] * n
 for i in range(n):
     g[i] = []
 for i in range(m):
     a,b=map(int,input().strip().split())
     g[a].append(b)
+    grados[b] += 1
 
-listaOrdenados = topSort(g)
+listaOrdenados = topSort(g,grados)
 
-print(listaOrdenados)
+print(*listaOrdenados)
