d65d04bd2ba8d32fa6609e3490b77c7b59361781
Sergio <sermmoba8@gmail.com>
2024-10-28 10:18:49 +0100

    velada done

--- Tema3/VeladaDelAno.py
+++ Tema3/VeladaDelAno.py
@@ -1,5 +1,18 @@
 
 
+
+def greedy(g):
+    ultima_actividad=-1
+    aux= 0
+    for i in g:
+        fin,inicio=i[0],i[2]
+        if inicio >= ultima_actividad:
+            aux +=1
+            ultima_actividad=fin
+    return aux
+
+
+
 # Data definition
 numActividades=input()
 numActividades =int(numActividades)
@@ -8,4 +21,8 @@ for _ in range(numActividades):
     n,i,f=input().strip().split()
     i= int(i)
     f= int(f)
-    actividades[_] = n
+    actividades.append((f,n,i))
+
+listaOrdenados=sorted(actividades)
+activities=greedy(listaOrdenados)
+print(activities)
