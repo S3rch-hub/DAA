7cec1f1fd69cf0852fe34053e6ef15ca5d3f41a6
smoralesssss <sermoba8@gmail.com>
2024-11-04 17:34:28 +0100

    Qshell data definition

---
+++ Tema3/Qshell.py
@@ -0,0 +1,17 @@
+
+# Objetivo: Maximizar las veces que se encienden las luces de la tentación
+
+# Data definition
+n = input()
+n = int(n)
+c = input()
+m = input()
+m = int(m)
+t = input()
+t= int(t)
+for _ in range(t):
+    o,b,i,k,tiempoSed=input()
+    b=int(b)
+    i=int(i)
+    k=int(k)
+    tiempoSed=int(tiempoSed)
