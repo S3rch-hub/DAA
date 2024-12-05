7194ff048cb65dba986104f5f361161d6720bb15
Sergio <sermmoba8@gmail.com>
2024-12-05 11:17:02 +0100

    ClearSoul done

--- Tema4/ClearSouls.py
+++ Tema4/ClearSouls.py
@@ -24,27 +24,27 @@ def binSearchRec(lista,nivel):
 n = int(input().strip())
 enemigos = list(map(int, input().strip().split()))
 casos = int(input().strip())
+
+poder = [0] * (n+1)
+for i in range(n):
+    poder[i+1] = poder[i] + enemigos[i]
+
 sol =[]
 
 
 for k in range(casos):
-    numEnemigos = 0
-    poder = 0
+
     nivelCaballero = int(input().strip())
     indice = binSearchRec(enemigos, nivelCaballero)
     i = 0
     if indice < 0:
-        indice = -indice-1
-        while i < indice:
-            numEnemigos += 1
-            poder += enemigos[i]
-            i+=1
+        indice = -indice -1
+        numEnemigos = indice
+        poderObtenido= poder[indice]
     else:
-        while i <= indice:
-            numEnemigos += 1
-            poder += enemigos[i]
-            i +=1
-    sol.append((numEnemigos,poder))
+        numEnemigos = indice + 1
+        poderObtenido = poder[indice+1]
+    sol.append((numEnemigos,poderObtenido))
 
 for j in range(len(sol)):
     print(sol[j][0],sol[j][1])
